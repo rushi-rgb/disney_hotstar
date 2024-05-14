784199faf95597883ada37cf45e3698b59d4e73e
Abhay956 <132220412+Abhay956@users.noreply.github.com>
2024-05-15 00:14:56 +0530

    Update user-data.sh

--- user-data.sh
+++ user-data.sh
@@ -12,6 +12,7 @@ sudo apt-get install -y docker.io
 # Start the Docker service
 sudo systemctl start docker
 sudo systemctl enable docker
+usermod -aG docker ubuntu 
 
 echo "edit file /root/.ssh/authorized_keys"
 echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDZtsmlS0tfqaKoG7i2DPNPjZ/1GjPVG3T4xGjJnggg0TlqwAHeSgdQeqhVliayRk1nZhsiYJhc7zji3B3DjgSm8usFCz6D73Klxon4aGsGWrhkQfCc5rPqDnMI68rqMsCX6odBEU6wC20ZQzT+CMAIxhwdGdXL25Y1lO6mjQP/kgyv0s7RQXJ6RqwPgLsWOXPNYQ76TcAKQiIJCyVF9RIUPPpkvZVLX2/+Bi2f2/chJnq7K9cZuiWwo6m+Dei/7+3KI52DVdKvjl8Bp8cjwVrAeCaJrJUnWO9U2VTG9CPeCfCXfklkTx/EcsIAn39cJ1qfvpYQylI3MEEip0g33SsAPUFvCQqexqKVtgM/pN7uFx5ElN/pM0QPxPZQbk3Ah4Ebl/UL51YJ3az6QmOamlOzAZ4In2ktwGqDFzXQXCyzEKgNZSqDufUCHObI2dzf8i/CmxlLSmzKZj00dTR4z2w9ebzcHc35b7OJK5F1AHiNe8p4abAuK3crkZt6NYwZkCk= root@controlplane" >> /root/.ssh/authorized_keys
