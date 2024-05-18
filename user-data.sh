f825ba261fc2e8c750d80ca67ea51fece3068659
Abhay956 <132220412+Abhay956@users.noreply.github.com>
2024-05-18 13:24:16 +0530

    Update user-data.sh

--- user-data.sh
+++ user-data.sh
@@ -15,7 +15,7 @@ sudo systemctl enable docker
 usermod -aG docker ubuntu 
 
 echo "edit file /root/.ssh/authorized_keys"
-echo "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGh/bRYQ7Wo9XX0zrDHE538ueOf1n8I1+K3no454tgf6 root@ip-172-31-61-61" >> /root/.ssh/authorized_keys
+echo "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIO8eBTiUEmw43QgJXF/+Bqqy7hLB3Zm7fWxSzIrhgFDw jenkins@ip-172-31-55-133" >> /root/.ssh/authorized_keys
 
 # Edit sshd_config file
 sudo sed -i 's/#PasswordAuthentication yes/PasswordAuthentication yes/g' /etc/ssh/sshd_config
